2d59ce9fe0858f1bdaa53ea875e1ed2f5d3ee15d
famingyuan <faming.yuan@163.com>
2019-06-25 11:37:42 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -44,8 +44,9 @@ const config = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.[hash:8].js',
         // 用于对路径进行补全 比如 / 则为根路径，当使用本地访问时 则无法访问，需要使用web访问
-        // 可以根据dist发布的目录 做相应的适配操作 默认情况下 配置为./
-        publicPath: "./"
+        // 可以根据dist发布的目录 做相应的适配操作 默认情况下 配置为/
+        // 务必注意： webpack-dev-server 情况下，不能配置为./或者其他路径，只能为默认值 /
+        // publicPath: "/abc"
     },
     module: {
         rules: [{
